de97aa02a7f43365311be817db1ed58a0d655390
user52 <user52@key.net.tr>
2019-01-31 10:13:50 +0300

    InstallmentPlanForMerchant Eklendi

---
+++ src/app/InstallmentPlanForMerchant.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user52
+ * Date: 31.01.2019
+ * Time: 10:10
+ */
+
+namespace ParamposLibrary;
+
+use ParamposLibrary\Config;
+use param\GetInstallmentPlanForMerchant;
+
+class InstallmentPlanForMerchant extends Config
+{
+    public function __construct()
+    {
+        $this->set();
+    }
+
+    public function send()
+    {
+        $query = new GetInstallmentPlanForMerchant($this->clientCode, $this->clientUsername, $this->clientPassword, $this->guid, $this->mode);
+        $query->send();
+
+        return $query->parse();
+    }
+}
--- test.php
+++ test.php
@@ -3,6 +3,14 @@
 include "vendor/autoload.php";
 
 use ParamposLibrary\TransactionQuery;
+use ParamposLibrary\TransactionAbstact;
+use ParamposLibrary\InstallmentPlanForMerchant;
 
-$x = new TransactionQuery( "", "1a34afa323356", "");
+//$x = new TransactionQuery( "", "1a34afa323356", "");
+//print_r($x->send());
+
+//$x = new TransactionAbstact( "21.01.2018 00:00:00", "22.01.2018 23:59:59");
+//print_r($x->send());
+
+$x = new InstallmentPlanForMerchant();
 print_r($x->send());
